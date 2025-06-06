1d860029d534d551e8fd3978c9d72a36fccc31bb
Kadir Goksal <136205319+KGoksal@users.noreply.github.com>
2025-06-06 16:46:15 -0500

    Update main.tf

--- modules/aks-cluster/modules/main.tf
+++ modules/aks-cluster/modules/main.tf
@@ -2,9 +2,9 @@ resource "azurerm_kubernetes_cluster" "aks" {
   name                = var.aks_cluster_name 
   location            = var.location 
   resource_group_name = var.resource_group_name 
-  dns_prefix          = var.dns_prefix
+  dns_prefix          = var.dns_prefix 
   kubernetes_version  = "1.21.2"
-  enable_rbac         = true
+  enable_rbac         = true 
 
   default_node_pool {
     name       = "default"
