9b1a840fea40075e68d694472cf4482ed4c5a005
Kadir Goksal <136205319+KGoksal@users.noreply.github.com>
2025-06-09 15:58:38 -0500

    Update main.tf

--- modules/aks-cluster/modules/main.tf
+++ modules/aks-cluster/modules/main.tf
@@ -1,4 +1,4 @@
-resource "azurerm_kubernetes_cluster" "aks" { 
+resource "azurerm_kubernetes_cluster" "aks" {  
   name                = var.aks_cluster_name 
   location            = var.location 
   resource_group_name = var.resource_group_name 
