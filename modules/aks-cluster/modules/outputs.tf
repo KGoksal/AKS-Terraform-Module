6f5a2d04c934d7df80aaa904c83414ce5809e9f5
Kadir Goksal <136205319+KGoksal@users.noreply.github.com>
2025-06-10 07:51:13 -0500

    Update outputs.tf

--- modules/aks-cluster/modules/outputs.tf
+++ modules/aks-cluster/modules/outputs.tf
@@ -6,7 +6,7 @@ output "aks_name" {
 output "kube_config" {
   description = "Base64-encoded kubeconfig for CLI access."
   value       = azurerm_kubernetes_cluster.aks.kube_config_raw
-}
+} 
 
 output "node_resource_group" {
   description = "The name of the Azure-managed node resource group."
