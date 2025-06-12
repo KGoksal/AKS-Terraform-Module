b38c0d34c860c32d89bc024cb714fb9a6270ef6b
Kadir Goksal <136205319+KGoksal@users.noreply.github.com>
2025-06-12 07:04:01 -0500

    Update main.tf

--- modules/aks-cluster/modules/main.tf
+++ modules/aks-cluster/modules/main.tf
@@ -9,8 +9,8 @@ resource "azurerm_kubernetes_cluster" "aks" {
   default_node_pool { 
     name       = "default"
     node_count = var.node_count
-    vm_size    = var.vm_size 
-    os_type    = "Linux"
+    vm_size    = var.vm_size  
+    os_type    = "Linux" 
     vnet_subnet_id = var.subnet_id 
   } 
  
