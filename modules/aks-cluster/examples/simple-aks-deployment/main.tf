dce3c7589e9fbbb2a0d1bc6324d9ac060679f376
Kadir Goksal <136205319+KGoksal@users.noreply.github.com>
2025-06-21 14:10:25 -0500

    Update main.tf

--- modules/aks-cluster/examples/simple-aks-deployment/main.tf
+++ modules/aks-cluster/examples/simple-aks-deployment/main.tf
@@ -6,6 +6,6 @@ module "aks" {
   dns_prefix          = "kadir"  
   node_count          = 2  
   vm_size             = "Standard_DS2_v2" 
-  subnet_id           = "/subscriptions/<subscription-id>/resourceGroups/rg-dev-kadir/providers/Microsoft.Network/virtualNetworks/vnet-dev/subnets/default" 
+  subnet_id           = "/subscriptions/<subscription-id>/resourceGroups/rg-dev-kadir/providers/Microsoft.Network/virtualNetworks/vnet-dev/subnets/default"  
 } 
  
