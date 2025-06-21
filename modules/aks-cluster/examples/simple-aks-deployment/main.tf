a6d01424414fb718a20ce65e1d5787ad62eeed81
Kadir Goksal <136205319+KGoksal@users.noreply.github.com>
2025-06-21 14:10:19 -0500

    Update main.tf

--- modules/aks-cluster/examples/simple-aks-deployment/main.tf
+++ modules/aks-cluster/examples/simple-aks-deployment/main.tf
@@ -2,7 +2,7 @@ module "aks" {
   source              = "../../modules/aks-cluster" 
   resource_group_name = "rg-dev-kadir" 
   location            = "East US"  
-  aks_cluster_name    = "kadir-aks" 
+  aks_cluster_name    = "kadir-aks"  
   dns_prefix          = "kadir"  
   node_count          = 2  
   vm_size             = "Standard_DS2_v2" 
