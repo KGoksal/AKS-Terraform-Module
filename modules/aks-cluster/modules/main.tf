14cdd4f0ac8c46f873349a32ae1c6f26d3604a46
Kadir Goksal <136205319+KGoksal@users.noreply.github.com>
2025-06-22 12:02:32 -0500

    Update main.tf

--- modules/aks-cluster/modules/main.tf
+++ modules/aks-cluster/modules/main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_kubernetes_cluster" "aks" {  
-  name                = var.aks_cluster_name 
-  location            = var.location  
+  name                = var.aks_cluster_name  
+  location            = var.location   
   resource_group_name = var.resource_group_name 
   dns_prefix          = var.dns_prefix  
   kubernetes_version  = "1.21.2" 
